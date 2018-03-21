c06ed638129333b0d4f296fcaeb03d82936f5df1
Rajendra <research@iocare.in>
2018-03-21 07:28:16 +0530

    Embedded Code

---
+++ embedded-software/GatewayhttpClient/GatewayhttpClient.ino
@@ -0,0 +1,245 @@
+/**
+   SmartBin Gateway
+
+    Created on: 20-03-2018
+    By: ioCare, Pune
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+const char* ssid = "iocareap";
+const char* password = "iocare2018";
+
+const byte numChars = 100;
+char receivedChars[numChars]; // an array to store the received data
+
+char url[100];
+char datas[100];
+char allVals[7];
+boolean newData = false;
+void setup() {
+
+  USE_SERIAL.begin(115200);
+  // USE_SERIAL.setDebugOutput(true);
+
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  USE_SERIAL.print("connecting to ");
+  USE_SERIAL.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    USE_SERIAL.print(".");
+  }
+  USE_SERIAL.println("");
+  USE_SERIAL.println("WiFi connected");
+  USE_SERIAL.println("IP address: ");
+  USE_SERIAL.println(WiFi.localIP());
+  dataPoster("started");
+}
+
+void loop() {
+
+  recvWithEndMarker();
+  showNewData();
+  ///delay(10000);
+  delay(10);
+}
+
+void recvWithEndMarker() {
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  while (USE_SERIAL.available() > 0 && newData == false) {
+    rc = USE_SERIAL.read();
+
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) {
+        ndx = numChars - 1;
+      }
+    }
+    else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void showNewData() {
+  if (newData == true) {
+    dataPoster(receivedChars);
+    newData = false;
+  }
+}
+float stringToDouble(String & str)  {
+  return atof( str.c_str() );
+}
+void dataPoster(char rc[])
+{
+  int lvl;
+  double wgt;
+  String lats;
+  String lngs;
+  String s = String(rc);
+  //11;3;1;0;49;26.881996;75.813400;400
+  //1-level
+  //2-wt
+  int commaIndex = s.indexOf(';');
+  //  Search for the next comma just after the first
+  int secondCommaIndex = s.indexOf(';', commaIndex + 1);
+  String firstValue = s.substring(0, commaIndex);//11
+
+  int thirdCommaIndex = s.indexOf(';', secondCommaIndex + 1);
+  int secondValue = s.substring(commaIndex + 1, secondCommaIndex).toInt();//3
+  String thirdValue = s.substring(secondCommaIndex + 1, thirdCommaIndex); ///1
+
+  int fourthCommaIndex = s.indexOf(';', thirdCommaIndex + 1);
+  String fourtValue = s.substring(thirdCommaIndex + 1, fourthCommaIndex); ///1
+
+  int fifthCommaIndex = s.indexOf(';', fourthCommaIndex + 1);
+  int fifthValue = s.substring(fourthCommaIndex + 1, fifthCommaIndex).toInt(); ///1
+
+  int sixthCommaIndex = s.indexOf(';', fifthCommaIndex + 1);
+  int sixthValue = s.substring(fifthCommaIndex + 1, sixthCommaIndex).toInt(); ///1
+  String url;
+  //url = "http://scare.iocare.in/input/post.json?";
+  url = "http://www.iocare.in/hack/post-bins/";
+  url += "1,";
+  /* if (secondValue == 1) {
+     lvl = sixthValue;
+     USE_SERIAL.print("level\n");
+     USE_SERIAL.println(lvl);
+     url += "lvl:";
+     url += lvl;
+    } else {
+     if (secondValue == 2) {
+       wgt = sixthValue;
+       USE_SERIAL.print("weight\n");
+       USE_SERIAL.println(wgt);
+       url += "wgt:";
+       url += wgt;
+     } else {
+       String ts;
+       int sixthCommaIndex = s.indexOf(';', fifthCommaIndex + 1);
+       ts = s.substring(fifthCommaIndex + 1, sixthCommaIndex); ///1
+       lats = stringToDouble(ts);
+       url += "lat:";
+       url += lats;
+       USE_SERIAL.println(lats);
+       int seventhCommaIndex = s.indexOf(';', sixthCommaIndex + 1);
+       ts = s.substring(sixthCommaIndex + 1, seventhCommaIndex); ///1
+       lngs = stringToDouble(ts);
+       url += ",lng:";
+       url += lngs;
+       USE_SERIAL.println(lngs);
+     }
+    }*/
+
+    if (secondValue == 1) {
+      lvl = sixthValue;
+      USE_SERIAL.print("level\n");
+      USE_SERIAL.println(lvl);
+      url += "lvl,";
+      url += lvl;
+    } else {
+      if (secondValue == 2) {
+        wgt = sixthValue;
+        USE_SERIAL.print("weight\n");
+        USE_SERIAL.println(wgt);
+        url += "wgt,";
+        url += wgt;
+      } else {
+        String ts;
+        int sixthCommaIndex = s.indexOf(';', fifthCommaIndex + 1);
+        lats = s.substring(fifthCommaIndex + 1, sixthCommaIndex); ///1
+        USE_SERIAL.println(lats);
+        //lats = stringToDouble(lats);
+        url += "pos,";
+        url += lats;
+        
+        int seventhCommaIndex = s.indexOf(';', sixthCommaIndex + 1);
+        lngs = s.substring(sixthCommaIndex + 1, seventhCommaIndex); ///1
+        USE_SERIAL.println(lngs);
+        //lngs = stringToDouble(ts);
+        url += ",";
+        url += lngs;
+        
+      }
+    }
+
+    /*
+      if (secondValue == 3) {
+       lvl = sixthValue;
+       USE_SERIAL.print("level\n");
+       USE_SERIAL.println(lvl);
+
+       url += lvl;
+       url += ",";
+       wgt = sixthValue;
+       USE_SERIAL.print("weight\n");
+       USE_SERIAL.println(wgt);
+
+       url += wgt;
+       url += ",";
+       String ts;
+       int sixthCommaIndex = s.indexOf(';', fifthCommaIndex + 1);
+       ts = s.substring(fifthCommaIndex + 1, sixthCommaIndex); ///1
+       lats = stringToDouble(ts);
+       url += lats;
+       url += ",";
+       USE_SERIAL.println(lats);
+       int seventhCommaIndex = s.indexOf(';', sixthCommaIndex + 1);
+       ts = s.substring(sixthCommaIndex + 1, seventhCommaIndex); ///1
+       lngs = stringToDouble(ts);
+       url += lngs;
+       USE_SERIAL.println(lngs);
+      }
+    */
+
+
+    //url += ",data:";
+    //url += rc;
+    url += "";
+
+    USE_SERIAL.print("[HTTP] begin...\n");
+    // configure traged server and url{"power1":100,"power2":200,"power3":300}
+    //http.begin("http://172.26.58.53/hack/post.php?data=%s", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
+    HTTPClient http;
+    //sprintf(url, 'http://scare.iocare.in/input/post?apikey=9c5b81163fe6882a8c23fe1a1a8c4b1f&id=0&data={"power1":"%s"}', rc);
+    http.begin(url); //HTTP
+    USE_SERIAL.printf("[HTTP] GET... url: %s\n", url.c_str());
+    USE_SERIAL.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        String payload = http.getString();
+        USE_SERIAL.println(payload);
+      }
+    } else {
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+
+  }
+
---
+++ embedded-software/SB1/SB1.ino
@@ -0,0 +1,186 @@
+
+#define MY_DEBUG
+#define MY_RADIO_NRF24
+#define GPS_SEND_INTERVAL 10000
+
+#define SKETCH_VERSION "v1.5"
+#define ID_S_DISTANCE 1
+#define ID_S_WEIGHT 2
+#define CHILD_ID_GPS 3
+#define ID_S_INFO 4
+static const int GPS_PIN = A0;
+static const uint32_t GPSBaud = 9600;
+// Offset hours adjustment from gps time (UTC)
+const int offset = 1;
+
+#include <SPI.h>
+#include <TimeLib.h>
+#include <MySensors.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+//distance
+#include <NewPing.h>
+#define TRIGGER_PIN  4  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     5  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 400 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+//distance
+
+//Load Cell
+/*
+  #include "HX711.h"
+  #define DOUT  6
+  #define CLK  7
+  HX711 scale(DOUT, CLK);
+  float calibration_factor = 260.40;//obtain it from caliberation program
+  //Load Cell*/
+  #define COIN_DISP_WEIGHT 20
+  long lastWT = 0;
+
+// TinyGPS++ is used for parsing serial gps data
+TinyGPSPlus gps;
+SoftwareSerial ss(GPS_PIN, A1);
+unsigned long lastGPSSent = 0;
+
+// Some buffers
+char latBuf[11];
+char lngBuf[11];
+char altBuf[6];
+char payload[30];
+char sz[64];
+String info;
+MyMessage msg_GPS(CHILD_ID_GPS, V_POSITION);
+MyMessage msg_S_DISTANCE(ID_S_DISTANCE, V_DISTANCE);
+MyMessage msg_S_WEIGHT(ID_S_WEIGHT, V_WEIGHT);
+MyMessage msg_S_INFO(ID_S_INFO, V_TEXT);
+void setup() {
+  ss.begin(GPSBaud);
+  //scale.tare();//resest to zero
+  Serial.println("Enabling GPS..");
+  ss.begin(115200);
+  ss.println("AT+GPS=1");
+  wait(100);
+  ss.begin(GPSBaud);
+}
+
+
+void presentation() {
+  //Send the sensor node sketch version information to the gateway
+  Serial.println("Presenting Sensors");
+  present(ID_S_DISTANCE, S_DISTANCE, "Distance Sensor");
+  present(CHILD_ID_GPS, S_GPS, "GPS");
+  present(ID_S_WEIGHT, S_WEIGHT, "WEIGHT");
+  present(ID_S_INFO, S_INFO, "Bin Info");
+}
+
+void loop()
+{
+
+  unsigned long currentTime = millis();
+
+  // Evaluate if it is time to send a new position
+  bool timeToSend = currentTime - lastGPSSent > GPS_SEND_INTERVAL;
+
+  // Read gps data
+  while (ss.available())
+    gps.encode(ss.read());
+
+  if (timeToSend) {
+    // Sync gps time with Arduino
+    updateTime();
+
+    // Send current gps location
+    if (gps.location.isValid() && gps.altitude.isValid()) {
+      // Build position and altitude string to send
+      dtostrf(gps.location.lat(), 1, 6, latBuf);
+      dtostrf(gps.location.lng(), 1, 6, lngBuf);
+      dtostrf(gps.altitude.meters(), 1, 0, altBuf);
+      sprintf(payload, "%s;%s;%s", latBuf, lngBuf, altBuf);
+
+      Serial.print(F("Position: "));
+      Serial.println(payload);
+
+      send(msg_GPS.set(payload));
+
+      Serial.print(F("GPS Time: "));
+      sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d", month(), day(), year(), hour(), minute(), second());
+      Serial.println(sz);
+      info = "";
+      getWeight();
+      getLevel();
+
+    } else {
+      if (millis() > 5000 && gps.charsProcessed() < 10)
+        Serial.println(F("No GPS data received: check wiring"));
+      else
+        Serial.println(F("No GPS data received yet..."));
+    }
+    lastGPSSent = currentTime;
+  }
+}
+
+void updateTime()
+{
+  TinyGPSDate d = gps.date;
+  TinyGPSTime t = gps.time;
+  if (d.isValid() && t.isValid()) {
+    // set the Time to the latest GPS reading if less then 0.2 seconds old
+    setTime(t.hour(), t.minute(), t.second(), d.day(), d.month(), d.year());
+    adjustTime(offset * SECS_PER_HOUR);
+    return;
+  }
+  Serial.println(F("Unable to adjust time from GPS"));
+}
+void getWeight()
+{
+
+  Serial.print("\tWeight:\t");
+  long w =  12;//scale.get_units(10);
+
+  long currentWT;
+  currentWT = w;
+  bool do_dispense = currentWT - lastWT > COIN_DISP_WEIGHT;
+
+  lastWT = currentWT;
+  Serial.println(w, 1);//get average of 10 readings
+
+  if (w > 82)
+  {
+    Serial.print("Garbage Added ");
+    info += "Garbage Added ";
+  } else
+  {
+    //do nothing
+  }
+
+  if (do_dispense)
+  {
+    Serial.print("Coin Dispensed ");
+    info += "Coin Dispensed ";
+    ///MoveForward();
+    //wait(600);
+    //StopMotor();
+  } else
+  {
+    //do nothing
+  }
+  send(msg_S_WEIGHT.set(w));//weight
+
+}
+
+void getLevel()
+{
+  Serial.println("Level is: " );
+  long d = sonar.ping_cm();
+  Serial.println(d);
+  if ((d < 21) && (d > 0))
+  {
+    Serial.print("Bin full ");
+    info += "Bin Full ";
+  } else
+  {
+    //do nothing
+  }
+  //Send All Info to GateWay
+  send(msg_S_DISTANCE.set(d));//distance
+}
